3933cba2c4123f4d98c8f0c1ea07be67b2c31d45
Alex Barros <alexbarros.dev@gmail.com>
2026-01-18 20:20:03 -0300

    Add Task repository

---
+++ src/main/java/com/alexdev/repositories/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.alexdev.repositories;
+
+import com.alexdev.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
